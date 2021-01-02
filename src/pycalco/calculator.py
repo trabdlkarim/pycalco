eefcae3abaf5bb88205e015323895918be0a9679
trabdlkarim <trabdlkarim@programmer.net>
2021-01-03 00:51:22 +0300

    make some changes

--- src/pycalco/calculator.py
+++ src/pycalco/calculator.py
@@ -1,2 +1,13 @@
-def add_one(number):
-    return number + 1
+"""This module is a collection of classes and 
+operations for the arithmetic expressions that 
+will come as input from the end user.
+"""
+
+import sys
+import cmd
+import click
+
+
+class Calco(object):
+    def __init__(self):
+        pass
