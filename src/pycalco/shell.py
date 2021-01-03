d4e29003e7995b92186290006c5850e42b00d9f2
trabdlkarim <trabdlkarim@programmer.net>
2021-01-03 04:03:54 +0300

    make some more changes

--- src/pycalco/shell.py
+++ src/pycalco/shell.py
@@ -1,15 +1,22 @@
 import sys
+import math
 import cmd
+import re
+
 import click
 
+
 GREEN = '\033[92m'
+RED = '\033[31m'
 END = '\033[0m'
 VERSION = "0.9.0"
 
+GLOBALS = {}
+  
 class PyCalcoShell(cmd.Cmd):
     intro = "\nWelcome to PyCalco shell.\n"
     intro += "Type 'help' or '?' to list all available commands.\n"
-    intro += "PyCalco " + VERSION + " --A Powerful Python Arithmetic Calculator\n\n"
+    intro += "PyCalco " + VERSION + " -- A Powerful Python Arithmetic Calculator\n\n"
     prompt = GREEN + "PyCalco [%i]: " + END
     
     def preloop(self):
@@ -21,22 +28,54 @@ class PyCalcoShell(cmd.Cmd):
         PyCalcoShell.prompt = GREEN + "PyCalco [%i]: " % self.cmd_count + END 
         
         return line.lower() 
-        
-    def do_eval(self, expression):
+    
+    def emptyline(self):
         pass
+    
+    def default(self,line):
+        self.do_eval(line)
+        
+    def do_shell(self,line):
+        self.do_assn(line)     
+    
+    def help_shell(self):
+        print("A synomym or alias for ASSN command.")
+        print("Usage: ! VAR = VAL")
+    
+    def do_eval(self, expression):
+        try:
+            print(eval(expression,GLOBALS))
+        except Exception:
+            print(RED + "SyntaxError" + END)
         
     def help_eval(self):
         print("Take an expression as arg and evaluate it.")
+        print("Usage: eval EXPR")
         
         
-    def do_assn(self, statment):
-        pass
-    
+    def do_assn(self, statement):
+        if '=' in statement:
+            l = statement.split('=')
+            
+            if len(l) == 2:
+                l[0] = l[0].strip()
+                l[1] = l[1].strip()
+                pattern = r"(\D+\d*)+"
+             
+                if re.match(pattern,l[0]):
+                    if re.match(r'\d+',l[1]):
+                        GLOBALS[l[0]] = int(l[1])
+                        print("%s = %d" % (l[0],int(l[1])))   
+                    
+        else:
+            print(RED + "SyntaxError" + END)
+        
     def help_assn(self):
-        print("Assign a value to a variable")
+        print("Assign a value to a variable.")
+        print("Usage: assn VAR = VAL")
     
     
-    def do_bye(self,arg):
+    def do_bye(self, arg):
         return True
     
     def help_bye(self):
@@ -47,14 +86,14 @@ class PyCalcoShell(cmd.Cmd):
         return self.do_bye(arg)
     
     def help_exit(self):
-        print("A synonym or alias for bye command.")
+        print("A synonym or alias for BYE command.")
 
     
     def do_quit(self,arg):
         return self.do_bye(arg)
     
     def help_quit(self):
-        print("A synonym or alias for bye command.")
+        print("A synonym or alias for BYE command.")
 
         
     def postloop(self):
