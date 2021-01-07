0ff33db2cbdbc91beffc3fe6e67876e020d42284
Touré A. Karim <trabdlkarim@programmer.net>
2021-01-07 15:33:13 +0300

    Update calco.py

--- src/pycalco/cmd/calco.py
+++ src/pycalco/cmd/calco.py
@@ -7,7 +7,7 @@ calc = PyCalco()
 
 @click.group(invoke_without_command=True)
 @click.pass_context
-@click.option('-v',"--version", default=False, help="Print PyCalco version")
+@click.option("-v/--version", default=False, help="Print PyCalco version")
 def run(ctx, version):
     if version:
         click.echo(ver)
