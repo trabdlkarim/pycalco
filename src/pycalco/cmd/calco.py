877dc9554ae20095b93b428c98f55d5e0468d62c
Touré A. Karim <trabdlkarim@programmer.net>
2021-01-07 12:12:47 +0300

    Update calco.py

--- src/pycalco/cmd/calco.py
+++ src/pycalco/cmd/calco.py
@@ -1,9 +1,16 @@
+
 import click
 
 @click.command()
 @click.option('--count/-c', default=1, help='Number of greetings.')
 @click.argument('name')
-def run(self, count, name):
+def run(count, name):
     for i in range(count):
         click.echo('Hi there, '+name)
 
+
+def eval(expr):
+    pass
+
+def sym(expr):
+    pass
