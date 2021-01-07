35eec3e7636de778f709e645667557a82546c0d5
Touré A. Karim <trabdlkarim@programmer.net>
2021-01-07 13:22:24 +0300

    Update calco.py

--- src/pycalco/cmd/calco.py
+++ src/pycalco/cmd/calco.py
@@ -1,16 +1,19 @@
 import click
 
-from pycalco import version
+from pycalco import version as ver
 from pycalco.calculator import PyCalco
 
 calc = PyCalco()
 
 @click.group()
 @click.option("--version/-v", default=False, help="Print PyCalco version")
-def run():
-    click.echo(version)
+@click.option("--interactive/-i", default=True, help="Lauch PyCalco in interactive mode")
+def run(version,interactive):
+    if version:
+        click.echo(ver)
+    else if interactive:
+        calc.run()?
     
-
 @run.command()
 @click.argument('expr')
 def eval(expr):
@@ -20,6 +23,7 @@ def eval(expr):
 @click.argument('expr')
 def sym(expr):
     calc.exec('sym ' + expr)
+
 
 if __name__ == '__main__':
     run()
