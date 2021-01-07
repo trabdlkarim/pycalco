c46ea81dbbdfbbdf025e2ef835d7246a27ba6c79
Touré A. Karim <trabdlkarim@programmer.net>
2021-01-07 14:00:39 +0300

    Update calco.py

--- src/pycalco/cmd/calco.py
+++ src/pycalco/cmd/calco.py
@@ -5,25 +5,33 @@ from pycalco.calculator import PyCalco
 
 calc = PyCalco()
 
-@click.group()
+@click.group(invoke_without_command=True)
+@click.pass_context
 @click.option("--version/-v", default=False, help="Print PyCalco version")
-@click.option("--interactive/-i", default=True, help="Lauch PyCalco in interactive mode")
-def run(version,interactive):
+def run(ctx, version):
     if version:
         click.echo(ver)
-    else if interactive:
-        calc.run()?
+        exit(0)
+    else if ctx.invoked_subcommand is None:
+        calc.run()
     
 @run.command()
 @click.argument('expr')
 def eval(expr):
+    """Evaluate arthimetically given expression"""
     calc.exec('eval ' + expr)
 
 @run.command()
 @click.argument('expr')
 def sym(expr):
+    """Compute symbolically given expression"""
     calc.exec('sym ' + expr)
+
+@run.command()
+def launch():
+    """Launch PyCalco in interactive mode"""
+    calc.run()
 
 
 if __name__ == '__main__':
-    run()
+    run(ctx={})
