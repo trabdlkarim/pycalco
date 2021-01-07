e084159eb5e3811e279026e8572def61d9c0d38a
Touré A. Karim <trabdlkarim@programmer.net>
2021-01-07 12:46:40 +0300

    Update calco.py

--- src/pycalco/cmd/calco.py
+++ src/pycalco/cmd/calco.py
@@ -9,10 +9,12 @@ def run():
     
 
 @run.command()
+@click.argument('expr')
 def eval(expr):
     calc.exec('eval ' + expr)
 
 @run.command()
+@click.argument('expr')
 def sym(expr):
     calc.exec('sym ' + expr)
 
