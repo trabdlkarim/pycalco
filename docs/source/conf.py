e1101b5e424c87af037b9c296d87b55bda604ef7
Touré A. Karim <trabdlkarim@programmer.net>
2021-01-08 03:21:37 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PyCalco'
-copyright = u'2020, Toure A. Karim & Contributors -'
+copyright = u'2020, Toure A. Karim & Contributors'
 author = u'Toure A. Karim'
 
 # The version info for the project you're documenting, acts as replacement for
