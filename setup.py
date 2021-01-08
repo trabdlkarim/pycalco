f6313a7c7b7f502f0a750d54d594a63fe8695455
Touré A. Karim <trabdlkarim@programmer.net>
2021-01-08 10:58:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='A powerful arithmetic expressions evaluator',  # Optional
+    description='A powerful arithmetic and symbolic expressions evaluator',  # Optional
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
@@ -147,7 +147,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['click'],  # Optional
+    install_requires=['click','sympy'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
